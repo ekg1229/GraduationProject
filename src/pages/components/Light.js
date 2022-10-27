bddfa43abd54e4d035b2c62c63e2481c8111d1d7
ekg1229 <ekg1229@naver.com>
2022-10-28 00:43:18 +0900

    실시간 척추 상태 신호등

--- src/pages/components/Light.js
+++ src/pages/components/Light.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import TrafficLight from "./TrafficLight";
+import axios from 'axios';
+
+/*
+export const Light = styled(Link)`
+@media screen and (max-width: 768px) {
+display: flex;
+align-items: center;
+padding: 0 2rem;
+  flex-direction: column;
+}`
+*/
 
 function Light(){
+  const [color, setColor] = useState([
+    {id:'1', value:"gray"},
+    {id:'2', value:"red"},
+    {id:'3', value:"yellow"}
+  ]); //신호등 색상 배열(gray, red, yellow, green)
+  let [data, setData] = useState([]); //axios data 저장
+
+  useEffect(()=>{
+    //1초마다 서버에 데이터 요청(POST)
+    let interval = setInterval(()=>axios.post("/api/graph/liveState").then(res=>{
+      console.log(res.data); //data
+      setData(res.data);
+      console.log("success"); //success
+    }), 1000)
+
+    setTimeout(()=>{
+      clearInterval(interval);
+    }, 10000);
+    /*
+    .catch(res=>{
+      console.log(res); //error
+    })
+    */
+  }, []);
+  
   return(
-    <div style={{display: "flex", width: "1000px", height: "300px", margin: "0 auto"}}>
-      <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
-      <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "yellow", margin: "auto"}}></div>
-      <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
+    <div style={{display: "flex", width: "1000px", height: "300px", margin: "0 auto", display: 'flex', flexDirection: 'row'}}>
+      {color.map((item)=>{return <TrafficLight key={item.id} color={item.color}/>})}
     </div>
   );
 }
--- src/pages/dashboard/DashboardLiveState.js
+++ src/pages/dashboard/DashboardLiveState.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
@@ -12,26 +11,49 @@ import Gachon from "../../assets/img/gachonlogo.png"
 
 import TrafficLight from 'react-trafficlight'
 import Navbarss from "../components/Navbar/Navbarss";
-import Footer from "../../components/Footer";
+import Footer from "../components/Navbar/Footer";
 
 import Light from "../components/Light";
 import Result_1 from "../components/Result_1";
+import {Helmet} from'react-helmet'
+
 
 //신호등 페이지
 export default () => {
   return (
     <>
+    <Helmet>
+      <title>척추요정 신호등 페이지</title>
+    </Helmet>
       <Row className="justify-content-md-center">
       <Col xs={12}  className="mb-1">
 <Navbarss/>
       </Col>
       </Row>
-      <div style={{display: "flex", alignItems: "center", width: "1000px", height: "300px", margin: "0 auto", borderRadius: "500px", background: "#000000"}}>
+
+
+      <Col xs={12} xl={12} className="mb-4" >
+        <Row>
+        <Col xs={12} xl={1} className="mb-4" ></Col>
+      <Col xs={12} xl={10} className="mb-4" style={{background: 'white'}}>
+        <h2></h2>
+      <div className="mb-4" style={{display: "flex", alignItems: "center", width: "1000px", height: "300px", margin: "0 auto", borderRadius: "500px", background: "#000000"}}>
         <Light/>
       </div>
       <div className="result" style={{width:"1000px", height:"220px", margin: "0 auto", marginTop: "10px"}}>
         <Result_1/>
       </div>
+      </Col>
+
+      <Col xs={12} xl={1} className="mb-4" ></Col>
+
+
+      </Row>
+      </Col>
+
+
+
+
       <Row className="justify-content-md-center">
       <Col xs={12}  className="mb-1">
 <Footer/>

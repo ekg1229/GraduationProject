f438d45951af7f3dd32d8006fdba03fa8b3ccfeb
ekg1229 <ekg1229@naver.com>
2022-10-29 17:55:58 +0900

    척추 신호등_신호등 제어

--- src/pages/components/TrafficLight.js
+++ src/pages/components/TrafficLight.js
@@ -1,30 +1,33 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 
-function TrafficLight(state){
-  if(state===1){//안정
+//상태에 따라 신호등 제어
+function TrafficLight(props){
+  const state = props.state;
+
+  if(state==="1"){//안정
     return(
-      <div style={{display: "flex", width: "1000px", height: "300px", margin: "0 auto", display: 'flex', flexDirection: 'row'}}>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "green", margin: "auto"}}></div>
+      <div style={{display:"flex", width:"1000px", height:"300px", margin:"0 auto", flexDirection: 'row'}}>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#510000", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#514E00", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"green", margin:"auto"}}></div>
       </div>
     )
   }
-  else if(state===2){//경고 자세
+  else if(state==="2"){//경고
     return(
-      <div style={{display: "flex", width: "1000px", height: "300px", margin: "0 auto", display: 'flex', flexDirection: 'row'}}>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "yellow", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
+      <div style={{display:"flex", width:"1000px", height:"300px", margin:"0 auto", flexDirection: 'row'}}>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#510000", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"yellow", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#0B5100", margin:"auto"}}></div>
       </div>
     )
   }
-  else{//위험 자세
+  else{//위험
     return(
-      <div style={{display: "flex", width: "1000px", height: "300px", margin: "0 auto", display: 'flex', flexDirection: 'row'}}>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "red", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background: "gray", margin: "auto"}}></div>
+      <div style={{display:"flex", width:"1000px", height:"300px", margin:"0 auto", flexDirection: 'row'}}>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"red", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#514E00", margin:"auto"}}></div>
+        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#0B5100", margin:"auto"}}></div>
       </div>
     )
   }

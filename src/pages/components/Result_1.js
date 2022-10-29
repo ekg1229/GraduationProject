e00069ba3a911babddae5857ed22c022c9c171cb
ekg1229 <ekg1229@naver.com>
2022-10-29 17:54:31 +0900

    척추 신호등_자세별 오늘의 한 마디

--- src/pages/components/Result_1.js
+++ src/pages/components/Result_1.js
@@ -2,40 +2,30 @@ import React from "react";
 
 //오늘의 한 마디 기능
 function Result_1(props){
-  if(props==="1"){//바른 자세
+  const state = props.state;
+
+  if(state==="1"){//안정
     return(
-      <div style={{width:"1000px", height:"200px", border: "3px solid black", background: "white"}}>
-        <div>
-          <h3 style={{marginTop: "33px", textAlign:"center"}}>[ 오늘의 한 마디 ]<br/>
-          =======================================</h3>
-        </div>
-        <div style={{marginTop: "10px", textAlign:"center", fontSize: "21px"}}>
-          <b>"잘했어요! 이대로 계속 유지해주세요!"</b>
+      <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
+        <div style={{fontSize:"2.5rem", color:"black"}}>
+        <b>"잘했어요! 이대로 계속 유지해주세요!"</b>
         </div>
       </div>
     );
   }
-  else if(props==="2"){//경고 자세
+  else if(state==="2"){//경고
     return(
-      <div style={{width:"1000px", height:"200px", border: "3px solid black", background: "white"}}>
-        <div>
-          <h3 style={{marginTop: "33px", textAlign:"center"}}>[ 오늘의 한 마디 ]<br/>
-          =======================================</h3>
-        </div>
-        <div style={{marginTop: "10px", textAlign:"center", fontSize: "21px"}}>
-          <b>"자세가 불안해요! 허리를 펴는 게 어떨까요?"</b>
+      <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
+        <div style={{fontSize:"2.5rem", color:"black"}}>
+        <b>"자세가 불안해요! 허리를 펴는 게 어떨까요?"</b>
         </div>
       </div>
     );
   }
-  else{//위험 자세
+  else{//위험
     return(
-      <div style={{width:"1000px", height:"200px", border: "3px solid black", background: "white"}}>
-        <div>
-          <h3 style={{marginTop: "33px", textAlign:"center"}}>[ 오늘의 한 마디 ]<br/>
-          =======================================</h3>
-        </div>
-        <div style={{marginTop: "10px", textAlign:"center", fontSize: "21px"}}>
+      <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
+        <div style={{fontSize:"2.5rem", color:"black"}}>
           <b>"위험해요! 바른 자세로 앉아주세요!"</b>
         </div>
       </div>

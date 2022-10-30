0511216825992eb77ca8f73baa4f7f0eead482d4
ekg1229 <ekg1229@naver.com>
2022-10-30 23:53:38 +0900

    척추 신호등_오늘의 한 마디 기능 수정

--- src/pages/components/Result_1.js
+++ src/pages/components/Result_1.js
@@ -12,9 +12,9 @@ function Result_1(props){
       <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
         <div style={{fontSize:"2.5rem", color:"black"}}>
         <b>잘했어요! 이대로 계속 유지해주세요!</b>
-        {Backforth ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</div>):(<></>)}
-        {LeftRight ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</div>):(<></>)}
-        {Accuracy ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</div>):(<></>)}
+        {!Backforth ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</div>):(<></>)}
+        {!LeftRight ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</div>):(<></>)}
+        {!Accuracy ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</div>):(<></>)}
         </div>
       </div>
     );
@@ -24,9 +24,9 @@ function Result_1(props){
       <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
         <div style={{fontSize:"2.5rem", color:"black"}}>
         <b>자세가 불안해요! 허리를 펴는 게 어떨까요?</b>
-        {Backforth ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</div>):(<></>)}
-        {LeftRight ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</div>):(<></>)}
-        {Accuracy ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</div>):(<></>)}
+        {!Backforth ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</div>):(<></>)}
+        {!LeftRight ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</div>):(<></>)}
+        {!Accuracy ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</div>):(<></>)}
         </div>
       </div>
     );
@@ -36,9 +36,9 @@ function Result_1(props){
       <div style={{display:"flex", borderRadius:"1rem", justifyContent:"center", alignItems:"center", width:"1000px", height:"200px", border:"1px black", boxShadow:"0 0 10px black", background:"#B0D9F2"}}>
         <div style={{fontSize:"2.5rem", color:"black"}}>
           <b>위험해요! 바른 자세로 앉아주세요!</b>
-          {Backforth ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</div>):(<></>)}
-          {LeftRight ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</div>):(<></>)}
-          {Accuracy ? (<div style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</div>):(<></>)}
+          {!Backforth ? (<span style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 앞뒤에 신경써주세요!</span>):(<></>)}
+          {!LeftRight ? (<span style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>허리 좌우를 신경써주세요!</span>):(<></>)}
+          {!Accuracy ? (<span style={{display:"flex",fontSize:"1.5rem", color:"black", justifyContent:"center", alignItems:"center"}}>움직임을 최소화해주세요!</span>):(<></>)}
         </div>
       </div>
     );

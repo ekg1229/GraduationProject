00ac7a9779f19a916988d8fe16e456543a83cc01
ekg1229 <ekg1229@naver.com>
2022-10-30 23:54:35 +0900

    사용 통계 페이지

--- src/pages/dashboard/DashboardStat.js
+++ src/pages/dashboard/DashboardStat.js
@@ -12,57 +12,76 @@ import Gachon from "../../assets/img/gachonlogo.png"
 import Navbarss from "../components/Navbar/Navbarss";
 import Footer from "../components/Navbar/Footer";
 
-import StatChart from "../charts/Stat";
+import StatChart from "../components/Stat";
 import Result_2 from "../components/Result_2";
 
 import {Helmet} from'react-helmet'
+import '../../App.css'
 
 //사용 통계 페이지
 export default () => {
   return (
-<>
-<Helmet>
+    <div className="App" style={{background: "#E0F2FE",}}>
+    <Helmet>
       <title>척추요정 사용 통계 페이지</title>
     </Helmet>
+
+      {/*Nav 영역*/}
       <Row className="justify-content-md-center">
-      <Col xs={12}  className="mb-1">
-<Navbarss/>
-      </Col>
+        <Col xs={12}  className="mb-1">
+          <Navbarss/>
+        </Col>
       </Row>
 
       {/*Nav 밑에 여백*/}
-      <Row xs={1} style={{height: "3vh"}}></Row>
-
+      <Row xs={1} style={{height: "12vh", fontSize: "2.9rem", textAlign:"center", alignItems: "center", justifyContent: "center", margin: "0 auto", color: "black"}}> <b> 사용통계 </b> </Row>
 
       <Col xs={12} xl={12} className="mb-4" >
         <Row>
-
+        <Col xs={12} xl={1} className="mb-4" ></Col>
+      
+        {/*사용통계 영역*/}
+        <Col xs={12} xl={10} className="mb-4" style={{background: '#F1F6F5'}}>
+        <h2></h2>
+        <div className="chart" style={{position:"realative", width:"1000px", height:"500px", margin: "0 auto"}}>
+          <StatChart/>
+        </div>
+        <div className="result" style={{width:"1000px", height:"320px", margin: "0 auto", marginTop: "10px"}}>
+          <Result_2/>
+        </div>
+        </Col>
 
         <Col xs={12} xl={1} className="mb-4" ></Col>
 
-
-      <Col xs={12} xl={10} className="mb-4" style={{background: '#F1F6F5'}}>
-      <h2></h2>
-      <div className="chart" style={{position:"realative", width:"1000px", height:"500px", margin: "0 auto"}}>
-        <StatChart/>
-      </div>
-      <div className="result" style={{width:"1000px", height:"320px", margin: "0 auto", marginTop: "10px"}}>
-        <Result_2/>
-      </div>
+        </Row>
       </Col>
 
-      <Col xs={12} xl={1} className="mb-4" ></Col>
-
-
-      </Row>
-      </Col>
-
-
+      {/*Footer 영역*/}
       <Row className="justify-content-md-center">
-      <Col xs={12}  className="mb-1">
-<Footer/>
-      </Col>
+        <Col xs={12}  className="mb-1" >
+    <Footer/>
+        </Col>
       </Row>
-    </>
+    </div>
   );
 };
+
+
+/*
+  function usePreventLeave() {
+    function listener(e) {
+      e.preventDefault();
+      e.returnValue = '';
+    }
+  
+    function enablePrevent() {
+      window.addEventListener('beforeunload', listener)
+    }
+  
+    function disablePrevent() {
+      window.removeEventListener('beforeunload', listener)
+    }
+  
+    return {enablePrevent, disablePrevent}
+  }
+*/

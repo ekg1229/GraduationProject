40e3b272428d1c9877b833c1a7cf8ed68059e4eb
ekg1229 <ekg1229@naver.com>
2022-11-01 03:31:29 +0900

    척추 신호등_자세에 따라 신호등 제어_최종

--- src/pages/components/TrafficLight.js
+++ src/pages/components/TrafficLight.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+
+
 //상태에 따라 신호등 제어
 function TrafficLight(props){
   const state = props.state;
@@ -25,9 +27,9 @@ function TrafficLight(props){
   else{//위험
     return(
       <div style={{display:"flex", width:"1000px", height:"300px", margin:"0 auto", flexDirection: 'row'}}>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"red", margin:"auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#514E00", margin:"auto"}}></div>
-        <div style={{id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#0B5100", margin:"auto"}}></div>
+        <div style={{display: "absoulte", id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"red", margin:"auto"}}></div>
+        <div style={{display: "absoulte", id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#514E00", margin:"auto"}}></div>
+        <div style={{display: "absoulte", id:"circle", width:"250px", height:"250px", borderRadius:"50%", background:"#0B5100", margin:"auto"}}></div>
       </div>
     )
   }

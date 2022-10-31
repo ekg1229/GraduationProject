0c44e1d21bd1dfb5c4bbd117f4bd75860c64aff9
ekg1229 <ekg1229@naver.com>
2022-11-01 03:33:14 +0900

    실시간 척추 신호등 페이지_최종

--- src/pages/dashboard/DashboardLiveState.js
+++ src/pages/dashboard/DashboardLiveState.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
+
 import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
 
-import { CounterWidget, CircleChartWidget, BarChartWidget, TeamMembersWidget, ProgressTrackWidget, RankingWidget, SalesValueWidget, SalesValueWidgetPhone, AcquisitionWidget } from "../../components/Widgets";
-import { PageVisitsTable } from "../../components/Tables";
-import { trafficShares, totalOrders } from "../../data/charts";
-import { Link } from "react-router-dom";
-import Gachon from "../../assets/img/gachonlogo.png"
-
-import TrafficLight from 'react-trafficlight'
 import Navbarss from "../components/Navbar/Navbarss";
 import Footer from "../components/Navbar/Footer";
 
@@ -33,17 +25,26 @@ export default () => {
       </Row>
 
       {/*Nav 밑에 여백*/}
-      <Row xs={1} style={{height: "12vh", fontSize: "2.9rem", textAlign:"center", alignItems: "center", justifyContent: "center", margin: "0 auto", color: "black"}}> <b> 당신의 척추 건강 신호등 </b> </Row>
+      <Row xs={1} style={{height: "15vh", fontSize: "2.9rem", textAlign: "center", alignItems: "center", justifyContent: "center", margin: "0 auto", color: "black"}}> <b> 당신의 척추 건강 신호등 </b> </Row>
       
-      {/*척추 신호등 영역*/}
-      <Col xs={12} xl={10} className="mb-4" style={{width: "auto", height: "auto", margin: "0 auto"}}>
-          <Light/>
+      <Col xs={12} xl={12} className="mb-4" >
+        <Row>
+        <Col xs={12} xl={1} className="mb-4" ></Col>
+
+        {/*척추 신호등 영역*/}
+        <Col xs={12} xl={1} className="mb-4" style={{width: "60vw", height: "55vh", alignItems: "center", justifyContent: "center", margin: "0 auto", background: '#F1F6F5', borderRadius: '15px', marginLeft: "10vw", marginRight: "10vh", }}>
+          <br/><Light/>
+        </Col>
+
+        <Col xs={12} xl={1} className="mb-4" ></Col>
+
+        </Row>
       </Col>
 
       {/*Footer 영역*/}
-      <Row className="justify-content-md-center">
+      <Row className="justify-content-md-center" style={{height: "15vh", background: "#E0F2FE",}}>
         <Col xs={12}  className="mb-1" >
-    <Footer/>
+          <Footer/>
         </Col>
       </Row>
     </div>

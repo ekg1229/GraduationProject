b123cc0707b3f1ed562e38593966788ec13b7ebf
ekg1229 <ekg1229@naver.com>
2022-11-01 03:34:12 +0900

    서비스 정보 페이지(대시보드와 병합)

--- src/pages/dashboard/DashboardInfo.js
+++ src/pages/dashboard/DashboardInfo.js
@@ -1,25 +1,23 @@
 
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
 
 import { CounterWidget, CircleChartWidget, BarChartWidget, TeamMembersWidget, ProgressTrackWidget, RankingWidget, SalesValueWidget, SalesValueWidgetPhone, AcquisitionWidget } from "../../components/Widgets";
-import { PageVisitsTable } from "../../components/Tables";
-import { trafficShares, totalOrders } from "../../data/charts";
-import { Link } from "react-router-dom";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
-import Gachon from "../../assets/img/gachonlogo.png"
 import Navbarss from "../components/Navbar/Navbarss";
-import Footer from "../../components/Footer";
+import Footer from "../components/Navbar/Footer";
 
+import {Helmet} from'react-helmet'
+import ReactPlayer from "react-player";
+import '../../App.css'
 
 //서비스 정보 페이지
-
-
 export default () => {
   return (
-    <>
+    <div className="App">
+          <Helmet>
+      <title>척추요정 서비스 정보 페이지</title>
+    </Helmet>
      <Row className="justify-content-md-center">
       <Col xs={12}  className="mb-1">
 <Navbarss/>
@@ -29,17 +27,10 @@ export default () => {
 
       <Row className="justify-content-md-center">
         
-        <Col xs={12} className="mb-4 d-sm-none">
-          <SalesValueWidgetPhone
-            title="Sales Value"
-            value="10,567"
-            percentage={10.57}
-          />
-        </Col>
         <Col xs={12} sm={6} xl={4} className="mb-4">
           <CounterWidget
-            category="Customers"
-            title="345k"
+            category="서비스 버전"
+            title="v1.2"
             period="Feb 1 - Apr 1"
             percentage={18.2}
             icon={faCheckCircle}
@@ -49,8 +40,8 @@ export default () => {
 
         <Col xs={12} sm={6} xl={4} className="mb-4">
           <CounterWidget
-            category="Revenue"
-            title="$43,594"
+            category="마지막 업데이트 일자"
+            title="2022.10.29.(토)"
             period="Feb 1 - Apr 1"
             percentage={28.4}
             icon={faCheckCircle}
@@ -59,42 +50,81 @@ export default () => {
         </Col>
 
         <Col xs={12} sm={6} xl={4} className="mb-4">
-          <CircleChartWidget
-            title="Traffic Share"
-            data={trafficShares} />
+        <CounterWidget
+            category="서비스 상태"
+            title="정상 작동중"
+            period="Feb 1 - Apr 1"
+            percentage={28.4}
+            icon={faCheckCircle}
+            iconColor="shape-tertiary"
+          />
         </Col>
       </Row>
+
+
+      <Row className="justify-content-md-center">
+
+      <Col xs={12} sm={6} xl={4} className="mb-4">
+      <ReactPlayer
+        className="player"
+        url={"https://www.youtube.com/watch?v=Z1Fgi5IP92E"}
+        width="580px"
+        heigth="470px"
+        playing={false}
+        muted={true}
+        controls={true}
+        style={{marginBottom: '20px'}}
+      />
+      </Col>
+
+      <Col xs={12} sm={6} xl={4} className="mb-4">
+            <ReactPlayer
+        className="player"
+        url={"https://www.youtube.com/watch?v=hUB0dBxot_U"}
+        width="580px"
+        heigth="470px"
+        playing={false}
+        muted={true}
+        controls={true}
+        style={{marginBottom: '20px'}}
+      />
+            </Col>
+
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+            <ReactPlayer
+        className="player"
+        url={"https://www.youtube.com/watch?v=Scgj6O9Ch4M"}
+        width="580px"
+        heigth="470px"
+        playing={false}
+        muted={true}
+        controls={true}
+        style={{marginBottom: '20px'}}
+      />
+            </Col>
+
+
+      </Row>
+      
+      <Row className="justify-content-md-center">
+        
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+          
+        </Col>
+
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+          
+        </Col>
+
+        <Col xs={12} sm={6} xl={4} className="mb-4">
+        </Col>
+      </Row>
+
+
 
       <Row>
-        <Col xs={12} xl={12} className="mb-4">
-          <Row>
-            <Col xs={12} xl={8} className="mb-4">
-              <Row>
-                <Col xs={12} className="mb-4">
-                  <PageVisitsTable />
-                </Col>
-              </Row>
-            </Col>
-
-            <Col xs={12} xl={4}>
-              <Row>
-                <Col xs={12} className="mb-4">
-                  <BarChartWidget
-                    title="주간 서비스 사용량"
-                    value={452}
-                    percentage={18.2}
-                    data={totalOrders} />
-                </Col>
-
-                
-
-                
-              </Row>
-            </Col>
-          </Row>
-        </Col>
         <Footer/>
       </Row>
-    </>
+    </div>
   );
 };
